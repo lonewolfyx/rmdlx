13fc2f93d7d086a87576196675d5dadc28da0572
lonewolfyx <877085893@qq.com>
2026-02-08 22:38:34 +0800

    feat(cli): add clack prompts integration for enhanced CLI experience

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,6 @@
+import { intro } from '@clack/prompts'
 import { createMain, defineCommand } from 'citty'
+import pc from 'picocolors'
 import { description, name, version } from '../package.json'
 
 const command = defineCommand({
@@ -8,10 +10,7 @@ const command = defineCommand({
         description,
     },
     setup() {
-        console.log('Setup')
-    },
-    cleanup() {
-        console.log('Cleanup')
+        intro(pc.bgCyan(` ${name} [v${version}]`))
     },
     run({ args }) {
         console.log(args)
